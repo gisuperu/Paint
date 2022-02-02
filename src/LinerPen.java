7761ddd990c501985c7f5570a8fc8a0308e42157
Gisuperu <i10576@nara.kosen-ac.jp>
2022-02-03 00:20:09 +0900

    作成済みの自由，直線，三角のpenをアップデーと

---
+++ src/LinerPen.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class LinerPen extends Pen{
+
+    public LinerPen(){
+        super();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e, Graphics2D g, ToolStatus status){
+        status.addPoints(new int[]{e.getX(), e.getY()});
+        if(status.getPointsSize() >= 2){
+            int[] start = status.popPoints();
+            int[] end = status.popPoints();
+            g.setColor(status.getColor());
+            g.setStroke(new BasicStroke(status.getBold()));
+            g.drawLine(start[0], start[1], end[0], end[1]);
+            status.clearPoints();
+        }
+    }
+
+    @Override
+    public void startUp() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "直線";
+    }
+}
